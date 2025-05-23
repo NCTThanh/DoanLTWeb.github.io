3ef1e573af924db2aacb5dbf64140b3fd23e0b71
NCTThanh <thanhdayroi3004@gmail.com>
2025-05-24 02:19:25 +0700

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,36 +1,36 @@
 // JavaScript External - Script cho slideshow
-let slideIndex = 1;
-showSlides(slideIndex);
+ let slideIndex = 1;
+        showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  if (slides[slideIndex-1]) {
-    slides[slideIndex-1].style.display = "block";
-  }
-  if (dots[slideIndex-1]) {
-    dots[slideIndex-1].className += " active";
-  }
-}
+        function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("slide");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            if (slides[slideIndex-1]) { // Đảm bảo phần tử tồn tại trước khi truy cập
+                slides[slideIndex-1].style.display = "block";
+            }
+            if (dots[slideIndex-1]) { // Đảm bảo phần tử tồn tại trước khi truy cập
+                dots[slideIndex-1].className += " active";
+            }
+        }
 
-// Tự động chuyển slide (tùy chọn)
-setInterval(function(){
-    plusSlides(1);
-}, 5000); // Chuyển slide sau mỗi 5 giây
+        // Tự động chuyển slide (tùy chọn)
+        setInterval(function(){
+            plusSlides(1);
+        }, 5000);
